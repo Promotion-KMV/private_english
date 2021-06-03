4a63150ab8920c48f450fe1d90662554e3dfa769
generalisimus <dander-2403@mail.ru>
2021-06-03 12:58:26 +0000

    ubpdate vue

---
+++ gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/home/vlad/english/myenv/bin/gunicorn'
+pythonpath = '/home/vlad/english/private_english'
+bind = '0.0.0.0:8001'
+workers = 3
+user = 'vlad'
+limit_request_fields = 32000
+limit_request_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=english.settings'
