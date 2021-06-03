e3c5b913a24bc3043850991e8bd20c72861d0c9e
Vlad <Promotion-kmv@yandex.ru>
2021-06-03 09:46:17 +0300

    update static sett.py

--- english/settings.py
+++ english/settings.py
@@ -173,7 +173,7 @@ DEFAULT_FROM_EMAIL = environ.get('DEFAULT_FROM_EMAIL')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
