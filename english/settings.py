21c287fcbc18bec8aa5270ace4816da2107e43ba
Vlad <Promotion-kmv@yandex.ru>
2021-06-03 09:35:20 +0300

    update settings

--- english/settings.py
+++ english/settings.py
@@ -173,11 +173,12 @@ DEFAULT_FROM_EMAIL = environ.get('DEFAULT_FROM_EMAIL')
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
