b03dbb3f474d53c574d35ac3899253f20c1bec44
generalisimus <dander-2403@mail.ru>
2021-06-28 08:28:44 +0000

    mod gitignore

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,10 @@
 command = '/home/vlad/english/myenv/bin/gunicorn'
 pythonpath = '/home/vlad/english/private_english'
 bind = '0.0.0.0:8001'
-workers = 3
+#bind = '178.154.244.182:8001'
+workers = 5
 user = 'vlad'
 limit_request_fields = 32000
 limit_request_field_size = 0
+timeout=240
 raw_env = 'DJANGO_SETTINGS_MODULE=english.settings'
