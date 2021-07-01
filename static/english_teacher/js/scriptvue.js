3b026c43975fe433ee0907d72a407e32713fa663
Vlad <Promotion-kmv@yandex.ru>
2021-07-01 21:45:28 +0300

    mod static

--- static/english_teacher/js/scriptvue.js
+++ static/english_teacher/js/scriptvue.js
@@ -137,7 +137,7 @@ const App = {
                         // 'Content-type': 'application/json'
                     },
                 })
-                .then(data => data.text())
+                // .then(data => data.text())
                 .then((response) => {
                     if (!response.ok) {
 
