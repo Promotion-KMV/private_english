ca9007edf3afd1dd4941d59d8957cf1ede1a6fab
Vlad <Promotion-kmv@yandex.ru>
2021-07-03 14:15:55 +0300

    test fetch

--- english_teacher/views.py
+++ english_teacher/views.py
@@ -146,11 +146,11 @@ def send_message(request, sub, email, message):
         from_email='privatenglishtutor@yandex.ru',
         to=['privatenglishtutor@yandex.ru',]
     )
-    try:
-        email.send(fail_silently=False)
-        return HttpResponse('Это сообщение успешно отправленно!')
-    except:
-        return HttpResponse('Сообщение не отправлено ошибка сервера')
+    # try:
+    email.send(fail_silently=False)
+    #     return HttpResponse('Это сообщение успешно отправленно!')
+    # except:
+    #     return HttpResponse('Сообщение не отправлено ошибка сервера')
     # logger.debug('hello!')
 
 
