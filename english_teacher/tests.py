6b6982c99fb06e720bda68808afa3141759c8df3
Vlad <Promotion-kmv@yandex.ru>
2021-08-13 15:25:54 +0300

    create class NewVisitorTest

--- english_teacher/tests.py
+++ english_teacher/tests.py
@@ -1,6 +1,24 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox(executable_path="/home/vlad/Django_projects/privatenglish/english/geckodriver")
+
 # driver_two = webdriver.Chrome("/home/vlad/test_TDD/chromedriver")
-browser.get('http://localhost:8000')
-assert 'репетитор' in browser.title
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path="/home/vlad/Django_projects/privatenglish/english/geckodriver")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('репетитор', self.browser.title)
+        self.fail('Закончить тест')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
