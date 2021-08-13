49a11272b3c0f90b568a3ca17f76166d39379b88
Vlad <Promotion-kmv@yandex.ru>
2021-08-13 18:41:14 +0300

    add functional_test.py

---
+++ english_teacher/functional_tests.py
@@ -0,0 +1,20 @@
+import unittest
+from selenium import webdriver
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path="/home/vlad/Django_projects/privatenglish/english/geckodriver")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('репетитор', self.browser.title)
+        self.fail('Закончить тест')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
--- english_teacher/tests.py
+++ english_teacher/tests.py
@@ -1,24 +1,9 @@
-import unittest
+from django.test import TestCase
+from django.urls import resolve
+from .views import *
 
-from selenium import webdriver
+class HomePageTest(TestCase):
 
-
-# driver_two = webdriver.Chrome("/home/vlad/test_TDD/chromedriver")
-
-
-class NewVisitorTest(unittest.TestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox(executable_path="/home/vlad/Django_projects/privatenglish/english/geckodriver")
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('репетитор', self.browser.title)
-        self.fail('Закончить тест')
-
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
